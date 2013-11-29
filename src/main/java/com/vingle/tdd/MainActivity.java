2244da842dd441e7cc361baf78e57263456e0eb3
동수 문 <donxu@naver.com>
2013-11-30 03:42:20 +0900

    new project

---
+++ src/main/java/com/vingle/tdd/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vingle.tdd;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
